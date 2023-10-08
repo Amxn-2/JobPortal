bfcb790c484a3114db0cb80d9401b22b24665e88
Amxn-2 <amansharmaa1205@gmail.com>
2023-10-08 16:12:26 +0530

    Search bar updated

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -2,33 +2,44 @@
 import React, { useState } from 'react';
 
 interface SearchBarProps {
-  onSearch: (query: string) => void;
+  onSearch: (query: string, jobType: string) => void;
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [query, setQuery] = useState('');
+  const [jobType, setJobType] = useState(''); // Initialize with an empty value
 
   const handleSearch = () => {
-    onSearch(query);
+    onSearch(query, jobType);
   };
 
   return (
-    <div className="fixed mx-auto left-52 right-52 my-40 rounded-lg bg-[#121212] p-4 h-72 flex flex-col justify-center items-center">
-      <div className="relative left-20 right-20 w-96 rounded-lg bg-[#333]">
+    <div className="fixed mx-auto left-2 right-2 md:left-52 md:right-52 my-40 rounded-lg bg-[#121212] p-4 h-auto md:h-72 flex flex-col justify-center items-center">
+      <div className="relative w-full md:w-[60rem] rounded-lg bg-[#333]" style={{ marginTop: '-100px' }}>
         <input
           type="text"
-          placeholder="Search..."
+          placeholder="Search for job..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          className="mt- py-2 px-4 rounded-lg bg-[#333] text-white border-none focus:ring-0"
+          className="px-4 w-full md:w-[60rem] bg-[#333] rounded-lg h-16"
         />
         <button
           onClick={handleSearch}
-          className="absolute right-0 top-0 bg-[#333] text-white py-2 px-4 rounded-lg border-none hover:bg-[#555]"
+          className="absolute right-5 top-2 bg-[#242121] text-white py-3 px-10 rounded-lg border-none hover:bg-[#555]"
         >
           Search
         </button>
       </div>
+      <select
+        value={jobType}
+        onChange={(e) => setJobType(e.target.value)}
+        className="mt-4 px-4 w-36 h-10 md:w-60 bg-[#333] text-white rounded-lg border-none"
+      >
+        <option value="">Select Type</option>
+        <option value="full-time">Full Time</option>
+        <option value="part-time">Part Time</option>
+        <option value="work-from-home">Work from Home</option>
+      </select>
     </div>
   );
 };
