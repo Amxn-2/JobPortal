c60fb767d7a828d869e984321d4def0023427e66
Amxn-2 <amansharmaa1205@gmail.com>
2023-10-08 10:02:30 +0530

    Searchbar updated

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from 'react';
 
 interface SearchBarProps {
@@ -13,23 +13,24 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   };
 
   return (
-    <div className="fixed mx-auto left-52 right-52 my-40 rounded-lg bg-[#121212] p-4 h-72">
-      <input
-        type="text"
-        placeholder="Search..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        className="w-10px py-2 px-4 bg-[#333] text-white rounded-none border-none focus:ring-0"
-      />
-      <button
-        onClick={handleSearch}
-        className="bg-[#333] text-white py-2 px-4 rounded-none border-none ml-4 hover:bg-[#555]"
-      >
-        Search
-      </button>
+    <div className="fixed mx-auto left-52 right-52 my-40 rounded-lg bg-[#121212] p-4 h-72 flex flex-col justify-center items-center">
+      <div className="relative left-20 right-20 w-96 rounded-lg bg-[#333]">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="mt- py-2 px-4 rounded-lg bg-[#333] text-white border-none focus:ring-0"
+        />
+        <button
+          onClick={handleSearch}
+          className="absolute right-0 top-0 bg-[#333] text-white py-2 px-4 rounded-lg border-none hover:bg-[#555]"
+        >
+          Search
+        </button>
+      </div>
     </div>
   );
 };
 
 export default SearchBar;
-
